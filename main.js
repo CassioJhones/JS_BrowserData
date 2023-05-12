c254e507d54f4460e8a61a33e8712cf504bb127d
Cassio Jhones <56178855+CassioJhones@users.noreply.github.com>
2023-05-12 02:14:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ itens.forEach((elemento) => {
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
-
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
   const existe = itens.find((elemento) => elemento.nome === nome.value);
@@ -21,16 +20,13 @@ form.addEventListener("submit", (evento) => {
   if (existe) {
     itemAtual.id = existe.id;
     atualizaElemento(itemAtual);
-
-    itens[existe.id] = itemAtual
+    itens[itens.findIndex((elemento) => elemento.id === existe.id)] = itemAtual;
   } else {
-    itemAtual.id = itens.length;
+    itemAtual.id = itens[itens.length - 1] ? itens[itens.length - 1].id : 0;
     criaElemento(itemAtual);
     itens.push(itemAtual);
   }
-
   localStorage.setItem("itens", JSON.stringify(itens));
-
   nome.value = "";
   quantidade.value = "";
 });
@@ -46,10 +42,34 @@ function criaElemento(item) {
 
   novoItem.innerHTML += item.nome;
 
+  novoItem.appendChild(botaoDeleta(item.id));
+
   lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
   document.querySelector("[data-id='" + item.id + "']").innerHTML =
     item.quantidade;
+}
+
+function botaoDeleta(id) {
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerText = "X";
+
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode, id);
+  });
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+  tag.remove();
+  // remover item do array
+  itens.splice(
+    itens.findIndex((elemento) => elemento.id === id),
+    1
+  );
+
+  localStorage.setItem("itens", JSON.stringify(itens));
 }
